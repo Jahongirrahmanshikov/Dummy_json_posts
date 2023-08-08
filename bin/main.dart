bcccac7bfe352e0d532ea70c188aadfafc1553b3
Jahongirrahmanshikov <jahonggir617@gmail.com>
2023-08-08 14:51:00 +0500

    Get all posts by user id

--- bin/main.dart
+++ bin/main.dart
@@ -3,8 +3,7 @@ import 'package:dummy_json_posts/models/post_mode.dart';
 import 'package:http/http.dart';
 
 void main(List<String> args) async {
-  Uri uri = Uri.parse(
-      'https://dummyjson.com/posts?limit=10&skip=10&select=title,reactions,userId');
+  Uri uri = Uri.parse('https://dummyjson.com/posts/user/5');
   Response response = await get(uri);
 
   if (response.statusCode == 200) {
