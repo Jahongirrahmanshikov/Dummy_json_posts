caf888b9e25b5b4d2164c4636d90f377a90e14d3
Jahongirrahmanshikov <jahonggir617@gmail.com>
2023-08-08 14:41:28 +0500

    Get a single post

--- bin/main.dart
+++ bin/main.dart
@@ -4,12 +4,12 @@ import 'package:dummy_json_posts/models/post_mode.dart';
 import 'package:http/http.dart';
 
 void main(List<String> args) async {
-  Uri uri = Uri.parse('https://dummyjson.com/posts');
+  Uri uri = Uri.parse('https://dummyjson.com/posts/1');
   Response response = await get(uri);
 
   if (response.statusCode == 200) {
     print("Success");
-    PostModel postModel = PostModel.fromJson(jsonDecode(response.body));
+    Post postModel = Post.fromJson(jsonDecode(response.body));
     print(postModel);
   } else {
     print("Error ${response.statusCode}");
