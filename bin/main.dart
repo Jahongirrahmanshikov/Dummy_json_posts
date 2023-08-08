3457352aa195bd54ffe496e6a2b6a4820d6c2a4f
Jahongirrahmanshikov <jahonggir617@gmail.com>
2023-08-08 15:05:07 +0500

    Delete a post

--- bin/main.dart
+++ bin/main.dart
@@ -1,18 +1,10 @@
 import 'package:http/http.dart';
 
 void main(List<String> args) async {
-  Uri uri = Uri.parse('https://dummyjson.com/posts/2');
-
-  Response response = await put(uri, body: {
-    "title": "Hello World",
-    "body": "This is a dummy post",
-    "userId": "1",
-    "tags": "dart, flutter",
-    "reactions": "100"
-  });
-
+  Uri uri = Uri.parse('https://dummyjson.com/posts/4');
+  Response response = await delete(uri);
   if (response.statusCode == 200) {
-    print("Success");
+    print(" Delete Successfull");
   } else {
     print("Error ${response.statusCode}");
   }
